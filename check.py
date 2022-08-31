461128e991a05edb6befedf9f98575cc18949f80
raukens <rauan.kumash@gmail.com>
2022-08-31 20:32:32 +0600

    переделана загрузка в JSON

--- check.py
+++ check.py
@@ -1,13 +1,26 @@
 import requests
+import json
+import random
 
 
 def get_monsters():
-    source = 'https://ragnarokapi.bravan.cloudns.cl/monsters/?page=1&limit=100'
-    response = requests.get(source)
+    # source = 'https://ragnarokapi.bravan.cloudns.cl/monsters/?page=1&limit=100'
+    # response = requests.get(source)
+    #
+    # if response.status_code == 200:
+    #     print('Список монстров получен')
+    #     return response
+    # else:
+    #     print("Не удается получить список монстров")
 
-    if response.status_code == 200:
-        print('Список монстров получен')
-        return response
-    else:
-        print("Не удается получить список монстров")
+    with open("monsters.json") as json_file:
+        data = json.load(json_file)
 
+    return data
+
+
+# list_mons = get_monsters()
+
+# random_monster = random.choice(get_monsters())
+# monster_name = random_monster['name']['en']
+# print(monster_name)
