b7623cfc2035cec26f49744137c9c58022f01401
raukens <rauan.kumash@gmail.com>
2022-08-31 20:36:59 +0600

    в input добавлены int

--- services.py
+++ services.py
@@ -8,26 +8,28 @@ def is_number_proper(number, *args):
         if number in args:
             break
         else:
-            number = input("Введенное значение некорректно, повторите ввод: ")
+            number = int(input("Введенное значение некорректно, повторите ввод: "))
     return number
 
 
 def player_choice():
-    x = input("Для начала новой игры введите - 1, Для продолжения предыдущей сессии введите - 2, \
-                      Для выхода введите - 3")
+    x = int(input(
+        "Для начала новой игры введите - 1, "
+        "Для продолжения предыдущей сессии введите - 2, "
+        "Для выхода введите - 3 "))
     return is_number_proper(x, 1, 2, 3)
 
 
 def check_choices():
-    choice = player_choice()
     nickname = input("Введите ник ")
+    choice = player_choice()
+    gamer = Player(nickname)
     try:
-        with open(f"'{nickname}.json'") as json_file:
+        with open(f"{nickname}.json") as json_file:
             data = json.load(json_file)
-            existing_file = 1
-    except:
-        existing_file = 0
-        print("Нет сохраненных сессий с указанным ником")
+            existing_file = True
+    except FileNotFoundError:
+        existing_file = False
 
     if choice == 3:
         exit()
@@ -36,8 +38,9 @@ def check_choices():
         gamer = Player(nickname)
     if choice == 2:
         if not existing_file:
-            y = input("У вас нет сохраненных сессий, для начала новой игры введите - 4 \
-                      Для выхода введите - 3 ")
+            y = int(input(
+                "У вас нет сохраненных сессий, для начала новой игры введите - 4 "
+                "Для выхода введите - 3 "))
             inner_choise = is_number_proper(y, 3, 4)
             if inner_choise == 3:
                 exit()
